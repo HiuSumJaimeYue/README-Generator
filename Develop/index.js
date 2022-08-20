5e9c54cd831d07c9b24e8a302a7680bcb4330e1d
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-08-19 22:22:04 -0400

    change HTML title

--- Develop/index.js
+++ Develop/index.js
@@ -20,7 +20,7 @@ function writeToFile(fileName, data) {
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
-      <title>Portfolio Demo</title>
+      <title>README Generator</title>
     </head>
   
     <body>
