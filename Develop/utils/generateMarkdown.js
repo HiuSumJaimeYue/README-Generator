5cf4328d64e59442541ce63debac59f714ec3975
Hiu Sum Jaime Yue <hiusumjaimeyue@cmail.carleton.ca>
2022-08-22 04:34:27 -0400

    change write index.html to README.md

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -5,21 +5,15 @@ function renderLicenseBadge(license) {
     return '';
   }
   if (license == 'MIT') {
-    return `
-        <img src="https://img.shields.io/badge/License-MIT-green.svg"
-        alt="MIT-badge"></img>
+    return `![MIT-badge](https://img.shields.io/badge/License-MIT-green.svg)
     `;
   }
   else if (license == 'Apache 2.0') {
-    return `
-        <img src="https://img.shields.io/badge/License-Apache-blue.svg"
-        alt="Apache2-badge"></img>
+    return `![Apache2-badge](https://img.shields.io/badge/License-Apache-blue.svg)
   `;
   }
   else if (license == 'GPL') {
-    return `
-        <img src="https://img.shields.io/badge/License-GPL-blue.svg"
-        alt="GPL-badge"></img>
+    return `![GPL-badge](https://img.shields.io/badge/License-GPL-blue.svg)
   `;
   } else {
     return "";
@@ -52,10 +46,10 @@ function renderLicenseSection(license) {
     return '';
   }
   return ` 
-        <p>Licensed under the 
-        <a href=${renderLicenseLink(license)} target="_blank" rel="noopener noreferrer">
-        ${license}</a>
-        license.</p>
+Licensed under the 
+<a href=${renderLicenseLink(license)} target="_blank" rel="noopener noreferrer">
+${license}</a>
+license.
 `;
 }
 
@@ -66,82 +60,44 @@ function generateMarkdown(readmeData) {
   }
 
   return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>README Generator</title>
-  </head>
+# ${readmeData.name} · ${renderLicenseBadge(readmeData.license)}              
 
-  <body>
-    <h1>${readmeData.name} · ${renderLicenseBadge(readmeData.license)}</h1>
-    </header>
+## Description       
+${readmeData.description}         
 
-    <main>
-      <div>
-        <h2>Description</h2>
-        <p>${readmeData.description}</p>
-      </div>
-      <div>
-        <h2>Table of Contents</h2>
-        <ul>
-          <li>
-            <a href="#installation">Installation</a>
-          </li>
-          <li>
-            <a href="#usage">Usage</a>
-          </li>
-          <li>
-            <a href="#license">License</a>
-          </li>
-          <li>
-            <a href="#contributing">Contributing</a>
-          </li>
-          <li>
-            <a href="#tests">Tests</a>
-          </li>
-          <li>
-            <a href="#questions">Questions</a>
-          </li>
-        </ul>
-      </div>
-      <div id="installation">
-        <h2>Installation</h2>
-        <p>${readmeData.installationInstructions}</p>
-      </div>
-      <div id="usage">
-        <h2>Usage</h2>
-        <p>${readmeData.usageInformation}</p>
-      </div>
-      <div id="license">
-        <h2>License</h2>
-        <p class="text-dark">&copy; ${new Date().getFullYear()} by ${readmeData.contactInfo.github}</p>
-        ${renderLicenseSection(readmeData.license)}
-      </div>
-      <div id="contributing">
-        <h2>Contributing</h2>
-        <p>${readmeData.contributionGuidelines}</p>
-      </div>
-      <div id="tests">
-        <h2>Tests</h2>
-        <p>${readmeData.testInstructions}</p>
-      </div>
-      <div id="questions">
-        <h2>Questions</h2>
-        <p>If you have any questions about the project, 
-        the github link and email address of the author are shown below. </p>
-        <p>Github: <a href="https://github.com/${readmeData.contactInfo.github}" 
-        target="_blank" rel="noopener noreferrer" >
-        GitHub</a> & Email: <a href="mailto:${readmeData.contactInfo.email}">
-        ${readmeData.contactInfo.email}
-      </a></p>
-      </div>
-    </main>
-  </body>
-  </html>
-  `;
+## Table of Contents               
+-[Installation](#installation)          
+-[Usage](#usage)          
+-[License](#license)          
+-[Contributing](#contributing)          
+-[Tests](#tests)        
+-[Questions](#questions)        
+
+## Installation         
+${readmeData.installationInstructions}               
+
+## Usage         
+${readmeData.usageInformation}
+
+## License         
+&copy; ${new Date().getFullYear()} by ${readmeData.contactInfo.github}         
+${renderLicenseSection(readmeData.license)}         
+
+## Contributing         
+${readmeData.contributionGuidelines}         
+
+## Tests         
+${readmeData.testInstructions}
+
+## Questions         
+If you have any questions about the project, 
+the github link and email address of the author are shown below.                   
+Github: <a href="https://github.com/${readmeData.contactInfo.github}" 
+target="_blank" rel="noopener noreferrer" >
+GitHub</a> & Email: <a href="mailto:${readmeData.contactInfo.email}">
+${readmeData.contactInfo.email}
+</a>
+`;
 }
 
 module.exports = generateMarkdown;
